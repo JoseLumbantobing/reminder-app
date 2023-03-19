4ab98b169d6aa5d83147935cc123c0d3f28e985c
Jose Lumbantobing <jolbntobing2@gmail.com>
2023-03-19 17:09:08 +0700

    Adding useState or state hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ReminderList from './components/ReminderList';
 import Reminder from './models/Reminder';
 import { title } from 'process';
 
-const reminders: Reminder[] = [
-  { id:1, title: 'Reminder1'}
-]
-
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([
+    { id:1, title: 'Reminder1'}
+  ]);
+
   return (
     <div className="App">
       <ReminderList items={reminders} />

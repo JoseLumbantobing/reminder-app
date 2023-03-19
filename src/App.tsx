6cafcf061c9395f20f935642b7a5ff7c9ce6d154
Jose Lumbantobing <jolbntobing2@gmail.com>
2023-03-19 22:41:20 +0700

    Adding useEffect and call the function get data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ReminderList from './components/ReminderList';
 import Reminder from './models/Reminder';
 import { title } from 'process';
+import ReminderServices from './services/Reminder';
 
 function App() {
-  const [reminders, setReminders] = useState<Reminder[]>([
-    { id:1, title: 'Reminder1'}
-  ]);
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+
+  useEffect(() => {
+    loadReminders();
+  }, []);
+
+  const loadReminders = async () => {
+    const reminders = await ReminderServices.getReminders();
+    setReminders(reminders);
+  }
 
   return (
     <div className="App">
